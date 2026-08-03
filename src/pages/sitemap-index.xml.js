8ce72e36f1b673156e820591713969ba56ee600c
Jutt Web Solutions <YOUR_GITHUB_EMAIL@gmail.com>
2026-08-03 13:03:48 -0700

    Initial commit

---
+++ src/pages/sitemap-index.xml.js
@@ -0,0 +1,19 @@
+import { getCollection } from 'astro:content';
+import { posts } from '../data/posts';
+
+export async function GET() {
+  const baseUrl = 'https://jutt.ai';
+  const pages = ['/', '/blog', '/about', '/contact', '/privacy'];
+  const postUrls = posts.map((post) => `/blog/${post.slug}`);
+  const urls = [...pages, ...postUrls];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls
+    .map((url) => `\n  <url>\n    <loc>${baseUrl}${url}</loc>\n  </url>`)
+    .join('')}\n</urlset>\n`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8'
+    }
+  });
+}
