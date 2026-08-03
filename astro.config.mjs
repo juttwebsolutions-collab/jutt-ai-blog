ff538c43157617c85a659f6dbc6396cb5d985628
Jutt Web Solutions <YOUR_GITHUB_EMAIL@gmail.com>
2026-08-03 13:55:49 -0700

    Enable Astro sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://splendorous-biscuit-76f9e8.netlify.app',
+  integrations: [sitemap()],
+});
