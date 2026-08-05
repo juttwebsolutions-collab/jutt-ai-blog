cf97b852a009776d1eaea6bd6ba73fb551691cdd
Jutt Web Solutions <YOUR_GITHUB_EMAIL@gmail.com>
2026-08-05 08:26:26 -0700

    Add Astro Sitemap and SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://juttaiblog.netlify.app',
+  site: 'https://juttaiblog.online',
   integrations: [sitemap()],
 });
